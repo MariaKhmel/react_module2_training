1c8f2d44bc8631b1f06c2d4a40adc29c9216208b
MariaKhmel <mariakhmel8@gmail.com>
2024-01-21 04:27:16 +0200

    state and events

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { CounterBox } from './Counter.styled';
+import { Value } from './Value';
+import { Controls } from './Controls';
 
 export class Counter extends Component{
 
@@ -10,13 +13,29 @@ export class Counter extends Component{
         value: this.props.initialValue,
 
     }
+
+    handleIncrement = () => {
+        this.setState(prevValue => ({
+            value: prevValue.value + 1,
+        }))
+    }
     
+    handleDecrement = () => {
+        this.setState(prevState => ({
+            value: prevState.value - 1
+        }))
+    }
 
     render() {
         return (
-            <>
-              
-            </>
+            <CounterBox>
+                <Value value={this.state.value} /> 
+                <Controls
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                />
+            </CounterBox>
+           
         )
     }
 
